c505a80e3f50c763a77f237af7fd88ba40ad6c00
Joshua <cutejosh2@gmail.com>
2022-09-29 13:08:24 +0100

    useEffect: learning the basics of use effect hook, cleanup function

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,23 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth)
+  const checkSize = () => {
+    setSize(window.innerWidth)
+  }
+  useEffect(() => {
+    window.addEventListener('resize', checkSize)
+    // clean up func
+    // return ()=>{
+    //   console.log("cleanup")
+    //   window.removeEventListener('resize', checkSize)
+    // }
+  }, [])
+  return <>
+    <h2>useEffect cleanup</h2>
+    <h1>window</h1>
+    <h2>{size}px</h2>
+  </>
 };
 
 export default UseEffectCleanup;
