8a06cc46ac918193c025459fdc6a58c951ba69e7
Joshua <cutejosh2@gmail.com>
2022-09-29 09:32:58 +0100

    useState: to clear items in array basic 1

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { useState } from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const deleteItem = () =>{
+    setPeople([])
+  }
+  const [people, setPeople] = useState(data)
+  return <>
+  {
+    people.map((person) => {
+      const {id, name} = person
+      return <div key={id} className="item">
+        <h4>{name}</h4>
+      </div>
+    })
+  }
+  <button className='btn' onClick={deleteItem}>Delete</button>
+  </>
 };
 
 export default UseStateArray;
