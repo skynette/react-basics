981a1ef5d956d3821e289b06271f5937a8d22c8b
Joshua <cutejosh2@gmail.com>
2022-09-29 10:28:23 +0100

    useState: working with prevStateValue in useState function

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -5,14 +5,32 @@ const UseStateCounter = () => {
   const reset = () => {
     setValue(0)
   }
-  return <div style={{margin: '4rem 0'}}>
+  const complexIncrease = () => {
+    setTimeout(()=>{
+      // setValue(value + 1)
+      // here prevStateVal is exactly the value in the previous state 
+      setValue((prevStateVal)=>{
+        return prevStateVal + 1
+      })
+    }, 2000)
+  }
+  return <>
     <h2>useState counter example</h2>
-    <h2>Regular counter</h2>
-    <h1>{value}</h1>
-    <button className='btn' onClick={(()=> setValue(value - 1))} >Decrease</button>
-    <button className='btn' onClick={(()=> setValue(value + 1))}>Increase</button>
-    <button className='btn' onClick={reset}>Reset</button>
-  </div>
+    
+    <div style={{ margin: '4rem 0' }}>
+      <h2>Regular counter</h2>
+      <h1>{value}</h1>
+      <button className='btn' onClick={(() => setValue(value - 1))} >Decrease</button>
+      <button className='btn' onClick={(() => setValue(value + 1))}>Increase</button>
+      <button className='btn' onClick={reset}>Reset</button>
+    </div>
+    
+    <div style={{ margin: '4rem 0' }}>
+      <h2>More complex counter</h2>
+      <h1>{value}</h1>
+      <button className='btn' onClick={complexIncrease}>increase later</button>
+      </div>
+  </>
 };
 
 export default UseStateCounter;
