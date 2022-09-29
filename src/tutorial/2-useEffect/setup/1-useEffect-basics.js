52aacca4b963d0cfb2ddfbd674f7a36933da899b
Joshua <cutejosh2@gmail.com>
2022-09-29 12:14:17 +0100

    useEffect: learning the basics of use effect hook 2

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,19 +3,19 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  const [value, setValue] = useState(0)
-  useEffect(()=>{
-    console.log("Call useEffect")
-    if (value >= 1){
-      document.title = `New messages(${value})`
-    }    
-  })
-  console.log("render component");
-  return <>
-    <h2>useEffect Basics</h2>
-    <h1>{value}</h1>
-    <button className='btn' onClick={()=>setValue(value + 1)}>click </button>
-  </>
+	const [value, setValue] = useState(0)
+	useEffect(() => {
+		console.log("Call useEffect")
+		if (value >= 1) {
+			document.title = `New messages (${value})`
+		}
+	}, [value])
+	console.log("render component");
+	return <>
+		<h2>useEffect Basics</h2>
+		<h1>{value}</h1>
+		<button className='btn' onClick={() => setValue(value + 1)}>click </button>
+	</>
 };
 
 export default UseEffectBasics;
