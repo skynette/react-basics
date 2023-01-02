4d31b91e34af7e1d861e22e76b7c5fe91e554fdc
Joshua <cutejosh2@gmail.com>
2023-01-02 17:52:07 +0100

    short circuit syntax

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -5,8 +5,15 @@ import React, { useState } from 'react';
 const ShortCircuit = () => {
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
+  const [text, setText] = useState('')
+  const firstValue = text || 'hello world'
+  const secondValue = text && 'hello world'
+  
 
-  return <h2>short circuit</h2>;
+  return <>
+    <h1>{firstValue}</h1>
+    <h1>value: {secondValue}</h1>
+  </>;
 };
 
 export default ShortCircuit;
