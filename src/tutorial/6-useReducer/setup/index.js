5de86ce886c8f91a7372be394fd9a20a353f3ac6
Joshua <cutejosh2@gmail.com>
2023-01-08 12:25:35 +0100

    reducers generally in separate module

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,25 +1,8 @@
 import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
-// reducer function
-const reducer = (state, action) => {
-  if (action.type === 'ADD_ITEM') {
-    const newPeople = [...state.people, action.payload]
-    return { ...state, people: newPeople, isModalOpen: true, modalContent: 'Item added' }
-  }
-  if (action.type === 'NO_VALUE') {
-    return { ...state, isModalOpen: true, modalContent: 'Please enter value' }
-  }
-  if (action.type === 'CLOSE_MODAL') {
-    return { ...state, isModalOpen: false, modalContent: '' }
-  }
-  if (action.type === 'REMOVE_ITEM') {
-    const newPeople = state.people.filter((person)=> person.id !== action.payload)
-    return { ...state, people: newPeople, isModalOpen: true, modalContent: 'Item Removed'}
-  }
-  throw new Error('No matching action type')
+import reducer from './reducer';
 
-}
 const defaultState = {
   people: [],
   isModalOpen: false,
---
+++ src/tutorial/6-useReducer/setup/reducer.js
@@ -0,0 +1,20 @@
+// reducer function
+const reducer = (state, action) => {
+	if (action.type === 'ADD_ITEM') {
+	  const newPeople = [...state.people, action.payload]
+	  return { ...state, people: newPeople, isModalOpen: true, modalContent: 'Item added' }
+	}
+	if (action.type === 'NO_VALUE') {
+	  return { ...state, isModalOpen: true, modalContent: 'Please enter value' }
+	}
+	if (action.type === 'CLOSE_MODAL') {
+	  return { ...state, isModalOpen: false, modalContent: '' }
+	}
+	if (action.type === 'REMOVE_ITEM') {
+	  const newPeople = state.people.filter((person)=> person.id !== action.payload)
+	  return { ...state, people: newPeople, isModalOpen: true, modalContent: 'Item Removed'}
+	}
+	throw new Error('No matching action type')
+  
+  }
+  export default reducer
