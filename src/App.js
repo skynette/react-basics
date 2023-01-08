4ef0834447ce33e839132753155ae7826c539b6d
Joshua <cutejosh2@gmail.com>
2023-01-08 11:23:43 +0100

    setup structure for useRedcer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from "./tutorial/6-useReducer/final/"
+import Setup from "./tutorial/6-useReducer/setup/"
 
 function App() {
   return (
--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -2,9 +2,42 @@ import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
-
 const Index = () => {
-  return <h2>useReducer</h2>;
+  
+  const [name, setName] = useState('')
+  const [people, setPeople] = useState(data)
+  const [showModal, setShowModal] = useState(false)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (name){
+      setShowModal(true)
+      setPeople([...people, {id:new Date().getTime().toString(), name}])
+      setName('')
+    }
+    else{
+      setShowModal(true)
+    }
+  }
+  return <>
+    {showModal && <Modal />}
+    <form onSubmit={handleSubmit} className="form">
+      <div>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <button type='submit'>Add person</button>
+      </div>
+    </form>
+    <div>
+      {people?.map((person) =>{
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        )
+      })}
+      {!people && <h4>No Record Available</h4>}
+    </div>
+  </>
 };
 
 export default Index;
