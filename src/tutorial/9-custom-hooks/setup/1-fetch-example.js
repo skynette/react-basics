2832aa3b6a55c457945043338a1e852cd36c1033
Joshua <cutejosh2@gmail.com>
2023-02-10 09:51:26 +0100

    start of react-router

--- src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -6,25 +6,26 @@ import { useFetch } from './2-useFetch'
 const url = 'https://course-api.com/javascript-store-products'
 
 const Example = () => {
-  const [loading, setLoading] = useState(true)
-  const [products, setProducts] = useState([])
+	console.log("initial rendering");
+	const [loading, setLoading] = useState(true)
+	const [products, setProducts] = useState([])
 
-  const getProducts = async () => {
-    const response = await fetch(url)
-    const products = await response.json()
-    setProducts(products)
-    setLoading(false)
-  }
+	const getProducts = async () => {
+		const response = await fetch(url)
+		const products = await response.json()
+		setProducts(products)
+		setLoading(false)
+	}
 
-  useEffect(() => {
-    getProducts()
-  }, [url])
-  console.log(products)
-  return (
-    <div>
-      <h2>{loading ? 'loading...' : 'data'}</h2>
-    </div>
-  )
+	useEffect(() => {
+		getProducts()
+	}, [url])
+
+	return (
+		<div>
+			<h2>{loading ? 'loading...' : 'data'}</h2>
+		</div>
+	)
 }
 
 export default Example
